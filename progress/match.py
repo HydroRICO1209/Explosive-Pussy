6857212416853690ae246117aac04aab3345b92a
HydroRICO <juanhanng1209@gmail.com>
2023-02-08 18:34:27 +0800

    Update match.py

--- progress/match.py
+++ progress/match.py
@@ -3,16 +3,14 @@ async def Match(ctx):
 
     cid = ctx.channel.id
     
-    player1id = await dbfunc.fetchValue('player1id', 'match', cid, 'matchid')
-    player2id = await dbfunc.fetchValue('player2id', 'match', cid, 'matchid')
-    player3id = await dbfunc.fetchValue('player3id', 'match', cid, 'matchid')
-    player4id  = await dbfunc.fetchValue('player4id', 'match', cid, 'matchid')
+    matchhostid = await dbfunc.fetchValue('matchhostid', 'match', cid, 'matchid')
+    matchstarted = await dbfunc.fetchValue('matchstarted', 'match', cid, 'matchid')
+    matchtotalplayer = await dbfunc.fetchValue('matchtotalplayer', 'match', cid, 'matchid')
     
     match = {
-        'player1id': player1id,
-        'player2id': player2id,
-        'player3id': player3id,
-        'player4id': player4id
+        'matchhostid': matchhostid,
+        'matchstarted': matchstarted,
+        'matchtotalplayer': matchtotalplayer,
     }
     
     return match
