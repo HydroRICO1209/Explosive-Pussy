48443c24c43f2f1f35d36c1b5bd4fabc59eea104
HydroRICO <juanhanng1209@gmail.com>
2023-02-09 12:53:56 +0800

    Update join.py

--- commando/join.py
+++ commando/join.py
@@ -21,7 +21,7 @@ class join(commands.Cog):
             #game started/ joined
             if playerlist['player1id'] == None:
                 await ctx.send(f'**{username}**, game has not been created.')
-            if userid in (playerlist['player1id'], playerlist['player2id'], playerlist['player3id'], playerlist['player4id']):
+            elif userid in (playerlist['player1id'], playerlist['player2id'], playerlist['player3id'], playerlist['player4id']):
                 await ctx.send(f'**{username}**, you are already in there')
             elif match['matchstarted'] == True:
                 await ctx.send(f'Too late **{username}**, game started')
