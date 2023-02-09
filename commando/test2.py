40acefa1786be9b1a8ef6653369462894679abc8
HydroRICO <juanhanng1209@gmail.com>
2023-02-09 21:37:53 +0800

    Update test2.py

--- commando/test2.py
+++ commando/test2.py
@@ -10,7 +10,7 @@ class test2(commands.Cog):
     @commands.cooldown(1, 1, commands.BucketType.user)
     async def test2(self, ctx, arg):
         if ctx.author.id == 757508305256972338:
-            shufflestart(ctx)
+            await shufflestart(ctx)
             await ctx.send('first try YOOOOOOOOO')
         else:
             await ctx.send('Would you mind fucking off?')
