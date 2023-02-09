da4de6c18b55968a81b4b3b15b33307361990fbe
HydroRICO <juanhanng1209@gmail.com>
2023-02-09 16:17:12 +0800

    Update quit.py

--- commando/quit.py
+++ commando/quit.py
@@ -17,14 +17,16 @@ class MyCog(commands.Cog):
         playerlist = await Playerlist(ctx)
         
         created = await self.bot.db.fetch('SELECT * FROM match WHERE matchid = $1', (cid))
-        await ctx.send(f'created: {created}')
+        
         if created == []:
             await ctx.send(f'**{username}**, game has not been created')
         else:
             if userid == playerlist['player1id']:
+                await ctx.send(1)
                 await dbfunc.setIntValue('player1id', 'playerlist', cid, 1, matchid)
                 await ctx.send(f'**{username}**, you had left the game successfully')
             elif userid == playerlist['player2id']:
+                await ctx.send(2)
                 await dbfunc.setIntValue('player2id', 'playerlist', cid, 1, matchid)
                 await ctx.send(f'**{username}**, you had left the game successfully')
             elif userid == playerlist['player3id']:
@@ -33,6 +35,7 @@ class MyCog(commands.Cog):
             elif userid == playerlist['player4id']:
                 await dbfunc.setIntValue('player4id', 'playerlist', cid, 1, matchid)
                 await ctx.send(f'**{username}**, you had left the game successfully')
+            await ctx.send('rip')
 
 
 async def setup(bot):
