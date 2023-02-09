8d79ee699cdbffa5b6fab0b662d9d5f0099b8f92
HydroRICO <juanhanng1209@gmail.com>
2023-02-09 18:28:38 +0800

    asd

---
+++ commando/monkeyballs.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+import discord
+
+
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.cooldown(1, 1, commands.BucketType.user)
+    async def monkeyballs(self, ctx):
+        nama = None
+        if userid == 564103314673762304:
+            nama = 'zyong'
+            
+        if nama == None:
+            await ctx.send(f"lemme guess, its {ctx.author.name}")
+        else:
+            await ctx.send(f"lemme guess, its {nama}")
+
+
+async def setup(bot):
+    await bot.add_cog(MyCog(bot))
