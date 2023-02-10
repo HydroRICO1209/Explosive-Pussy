4b4403c04520fbe891bc65d9dc04b1181d88bbf7
HydroRICO <juanhanng1209@gmail.com>
2023-02-10 09:00:08 +0800

    Update shuffle.py

--- progress/shuffle.py
+++ progress/shuffle.py
@@ -4,13 +4,13 @@ from progress.itemlist import card_list
 
 async def shufflestart(ctx):
     dbfunc = ctx.bot.database_handler
-    deck = await Deck(ctx)
     userid = ctx.author.id
     cid = ctx.channel.id
     is_done = False
     
     for card in card_list:
         while is_done == False:
+            deck = await Deck(ctx)
             randnum = random.randint(1,8)
             if deck[f'card{randnum}'] == 'rip bozo':
                 item = f'card{randnum}'
