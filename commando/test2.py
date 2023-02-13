f74fb3ac2bee53ad1ec3d8b96f98b381aa9a622a
HydroRICO <juanhanng1209@gmail.com>
2023-02-13 12:53:23 +0800

    Update test2.py

--- commando/test2.py
+++ commando/test2.py
@@ -18,6 +18,11 @@ class test2(commands.Cog):
             answer = await self.bot.db.fetchval(fetch_query, cid)
             await ctx.send(f'1)datatype: {type(answer)}, data: {answer}')
             await ctx.send(f'2)datatype: {type(answer[0])}, data: {answer[0]}')
+            
+            fetch_query = f'SELECT fuckyou FROM test WHERE matchid = $1'
+            answer = await self.bot.db.fetchval(fetch_query, cid)
+            await ctx.send(f'3)datatype: {type(answer)}, data: {answer}')
+            await ctx.send(f'4)datatype: {type(answer[0])}, data: {answer[0]}')
         else:
             await ctx.send('Would you mind fucking off?')
 async def setup(bot):
