f72d3c77aab327d8de96b980df88e690b9451f01
HydroRICO <juanhanng1209@gmail.com>
2023-02-14 18:41:43 +0800

    Update test2.py

--- commando/test2.py
+++ commando/test2.py
@@ -19,7 +19,7 @@ class test2(commands.Cog):
             await ctx.send(f'1)datatype: {type(answer)}, data: {answer}')
             await ctx.send(f'2)datatype: {type(answer[0])}, data: {answer[0]}')
             
-            idk = ['1234', '2345', '3456']
+            idk = [1234, 2345, 3456]
             await dbfunc.setIntValue('fuck', 'test', cid, idk, 'matchid')
             
             fetch_query = f'SELECT fuck FROM test WHERE matchid = $1'
