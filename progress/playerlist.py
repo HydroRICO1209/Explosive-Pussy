e09fbf09e64bb122859728e23d526e420b2271ff
HydroRICO <juanhanng1209@gmail.com>
2023-02-14 18:59:08 +0800

    Update playerlist.py

--- progress/playerlist.py
+++ progress/playerlist.py
@@ -2,7 +2,7 @@ async def Playerlist(ctx):
     dbfunc = ctx.bot.database_handler
     cid = ctx.channel.id
     
-    player_list = await dbfunc.fetchValue('player_cardlist', 'playerlist', cid, 'matchid')
+    player_list = await dbfunc.fetchValue('userid', 'playerlist', cid, 'matchid')
     
     playerlist = {
         'player_list': player_list
